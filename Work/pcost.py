4b930fdbe5b0c4ff7544a7dc364c552cbf792907
gilbert <gtmiao99@gmail.com>
2021-06-25 20:44:04 -0700

    Handle error caused by missing value in pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -8,10 +8,13 @@ def portfolio_cost(filename):
     with open(filename, "rt") as f:
         header = next(f)
         for line in f:
-            row = line.split(",")
-            shares = int(row[1])
-            price = float(row[2])
-            cost += shares * price
+            try:
+                row = line.split(",")
+                shares = int(row[1])
+                price = float(row[2])
+                cost += shares * price
+            except ValueError:
+                print("Invalid literal in line:", line)
     return cost
 
 cost = portfolio_cost("Data/portfolio.csv")
