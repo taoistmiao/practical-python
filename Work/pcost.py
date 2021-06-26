28977476676fda7302b1d392bf7a554bfcb3c89b
gilbert <gtmiao99@gmail.com>
2021-06-25 20:10:14 -0700

    Finish pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+cost = 0
+with open("Data/portfolio.csv", "rt") as f:
+    header = next(f)
+    for line in f:
+        row = line.split(",")
+        shares = int(row[1])
+        price = float(row[2])
+        cost += shares * price
+print("Total cost", round(cost, 2))
+
