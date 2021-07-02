057ec9aed50bfd509d6b0745ad055e5c785e8859
gilbert <gtmiao99@gmail.com>
2021-07-01 21:22:04 -0400

    Finish basic function of report.py

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,18 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    """Read a portfolio into a list of tuples"""
+    portfolio = []
+
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+
+    return portfolio
+    
