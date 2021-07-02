96e6cf8d4eb3bf4c5dee6fe75872f9de6d58fb1d
gilbert <gtmiao99@gmail.com>
2021-07-01 21:40:38 -0400

    Modify report.py to return a list of dictionaries

--- Work/report.py
+++ Work/report.py
@@ -5,14 +5,24 @@ import csv
 
 def read_portfolio(filename):
     """Read a portfolio into a list of tuples"""
+    # portfolio = []
+
+    # with open(filename, "rt") as f:
+    #     rows = csv.reader(f)
+    #     header = next(rows)
+    #     for row in rows:
+    #         holding = (row[0], int(row[1]), float(row[2]))
+    #         portfolio.append(holding)
     portfolio = []
 
     with open(filename, "rt") as f:
         rows = csv.reader(f)
         header = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {}
+            holding["name"] = row[0]
+            holding["shares"] = int(row[1])
+            holding["price"] = float(row[2])
             portfolio.append(holding)
 
     return portfolio
-    
