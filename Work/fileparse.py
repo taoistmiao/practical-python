3de6c58daf13041f3ada125e999da31d1191a007
gilbert <gtmiao99@gmail.com>
2021-07-03 16:18:39 -0400

    Add column selector feature (3.4)

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,28 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+def parse_csv(filename: str, select: list = []) -> "list[dict]":
+    """
+    Parse a csv file into a list of records
+    """
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+
+        headers = next(rows)
+        if select:
+            indices = [headers.index(header) for header in select]
+            headers = select
+
+        records = []
+        for row in rows:
+            # skip blank lines
+            if not row:
+                continue
+            # exclude unselected entries
+            row = [row[idx] for idx in indices]
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
